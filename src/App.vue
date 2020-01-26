2f4f216d51e1062a4c17b543450398380ed5ba02
root <root@DESKTOP-6545LIF.localdomain>
2020-01-26 04:08:22 +0000

    -Complete a setup for the nav with Boot strap and got some views working

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,37 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/contact">Contact</router-link>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Articles
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <router-link class="dropdown-item" to="/">Action</router-link>
+              <router-link class="dropdown-item" to="/">Another action</router-link>
+              <div class="dropdown-divider"></div>
+              <router-link class="dropdown-item" to="/">Something else here</router-link>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
     </div>
     <router-view/>
   </div>
---
+++ src/views/Contact.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="Contact">
+    <h1>This is a Contact page</h1>
+  </div>
+</template>
